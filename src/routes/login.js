b2a93d53396cec31ac3d2b5392c650a5b1e076bf
dahi0 <medmed16yl4@gmail.com>
2026-08-11 17:43:30 +0000

    added the login logic

--- src/routes/login.js
+++ src/routes/login.js
@@ -10,4 +10,25 @@ module.exports = [
       });
     }
   },
+  {
+    method: 'POST',
+    path: '/login',
+    handler: async(req, h) => {
+      const u = await User.findOne({
+        where: {
+          email: req.payload.email
+        }
+      });
+      if (u == null) return h.view('login', {
+        error: true,
+        errMessage: 'user not found'
+      });
+      if (!(await u.validPassword(req.payload.password))) return h.view('login', {
+        error: true,
+        errMessage: 'invalid password'
+      });
+      req.cookieAuth.set({ id: u.id });
+      return h.redirect('/');
+    }
+  }
 ];
